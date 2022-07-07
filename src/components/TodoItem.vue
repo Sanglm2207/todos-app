e4faa5f77d891adac8323493152195328c473e61
Lại Minh Sáng <Sanglm2207@gmail.com>
2022-07-07 21:10:36 +0700

    first commit

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,31 @@
+<template>
+  <p :class="[todo-item, todoProps.completed ? 'is-completed': '']">
+    <input  class="form-check-input" type="checkbox" :checked="todoProps.completed"/>
+    {{ todoProps.title }}
+    <button type="button" class="btn btn-danger btn-del">Delete</button>
+  </p>
+</template>
+
+<script>
+
+export default {
+    name: 'TodoItem',
+    props: ['todoProps'],
+   
+}
+</script>
+
+<style>
+.todo-item {
+  background: #dba7a7;
+  padding: 10px;
+  margin: 0;
+  border-bottom: 1px rgb(119, 119, 119) dotted;
+}
+.is-completed {
+  text-decoration: line-through;
+}
+.btn-del {
+  float: right;
+}
+</style>
---
+++ src/components/Todos.vue
@@ -0,0 +1,39 @@
+<template>
+    <TodoItem v-for="todo in todos" v-bind:key="todo.id" v-bind:todoProps="todo" />
+</template>
+
+<script>
+import { ref } from 'vue'
+import TodoItem from './TodoItem'
+
+
+export default {
+    name: 'Todos',
+    components: { TodoItem },
+    setup() {
+        const todos = ref([
+            {
+                id: 1,
+                title: "Todo 1",
+                completed: false
+            },
+            {
+                id: 2,
+                title: "Todo 2",
+                completed: true
+            },
+            {
+                id: 3,
+                title: "Todo 3",
+                completed: true
+            }
+        ])
+        return {
+            todos
+        }
+    }
+}
+</script>
+
+<style>
+</style>
